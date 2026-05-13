7f210de1d0b2a9ed677be0e297fb1cec528a8e7e
OsmanBeratPasa <osmanberatpasa@gmail.com>
2026-05-13 10:38:29 +0300

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0;
+        int min = prices[0];
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]<min){
+                min = prices[i];
+            }
+            else{
+                int fark = prices[i]-min;
+                if(fark>max){
+                    max = fark;
+                }
+            }
+
+        }
+return max;
+
+    }
+};
